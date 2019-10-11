95657c6dea8ab6285c69c7a269a8dced790e49e3
Lauren <Lauren.Musante@gmail.com>
2019-10-11 08:32:46 -0700

    Add boilerplate content to project.

---
+++ Bakery2/Controllers/VendorController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Places.Models;
+using System.Collections.Generic;
+
+namespace Bakery.Controllers
+{
+    public class BakeryController : Controller
+    {
+    }
+}
